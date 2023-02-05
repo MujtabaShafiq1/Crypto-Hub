cb4b1598910e7cd530dc7be75a4041db5d7a7e5a
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-02-06 01:01:13 +0500

    added local storage token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,52 +14,43 @@ const login = asyncHandler(async (req, res, next) => {
     const validPassword = await bcrypt.compare(req.body.password, password)
     if (!validPassword) return next(createError(400, "Incorrect Password"));
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: "1d" })
+    const token = jwt.sign({ id: user._id, userId }, process.env.JWT_KEY, { expiresIn: "1d" })
     res.status(200).json({ token, userId, name, photo })
 })
 
+
 const register = asyncHandler(async (req, res, next) => {
-    try {
 
-        jwt.verify(req.body.token, process.env.JWT_EMAIL_KEY)
+    jwt.verify(req.body.token, process.env.JWT_EMAIL_KEY)
 
-        const user = await Tokens.findOne({ where: { token: req.body.token } })
-        const { id, token, ...otherDetails } = user.dataValues
+    const user = await Tokens.findOne({ where: { token: req.body.token } })
+    const { id, token, ...otherDetails } = user.dataValues
 
-        await Tokens.destroy({ where: { userId: otherDetails.userId } })
-        await Users.create(otherDetails)
+    await Tokens.destroy({ where: { userId: otherDetails.userId } })
+    await Users.create(otherDetails)
 
-        res.status(201).json("Success")
+    res.status(201).json("Success")
 
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({ message: (e.message || "Please try again later") })
-    }
 })
 
-
 const socialLogin = asyncHandler(async (req, res, next) => {
-    try {
 
-        if (!req.user) return res.status(404).json({ message: "Authentication Failed" })
+    if (!req.user) return res.status(404).json({ message: "Authentication Failed" })
 
-        if (req.user.provider === "local") return res.status(200).json(req.user);
+    if (req.user.provider === "local") return res.status(200).json(req.user);
 
-        const foundUser = await Users.findOne({ where: { userId: (req.user._json.email || req.user.id) } })
+    const foundUser = await Users.findOne({ where: { userId: (req.user._json.email || req.user.id) } })
 
-        if (foundUser) {
-            const { password, ...details } = foundUser.dataValues
-            return res.status(200).json(details);
-        }
-
-        const { id, displayName, photos, provider } = req.user;
-        const user = { userId: (req.user._json.email || id), name: displayName, photo: (provider === "steam" ? photos[2].value : photos[0].value), provider }
-        await Users.create(user)
-        res.status(200).json(user);
-
-    } catch (e) {
-        res.status(500).json({ message: "Please try again later" })
+    if (foundUser) {
+        const { password, ...details } = foundUser.dataValues
+        return res.status(200).json(details);
     }
+
+    const { id, displayName, photos, provider } = req.user;
+    const user = { userId: (req.user._json.email || id), name: displayName, photo: (provider === "steam" ? photos[2].value : photos[0].value), provider }
+    await Users.create(user)
+    res.status(200).json(user);
+
 })
 
 module.exports = { login, register, socialLogin };
--- server/controllers/token.js
+++ server/controllers/token.js
@@ -6,28 +6,22 @@ const jwt = require("jsonwebtoken")
 
 const tempUser = async (req, res, next) => {
 
-    try {
+    const { name, email, password, file } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-        const { name, email, password, file } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10)
+    await Tokens.destroy({ where: { createdAt: { [Op.lt]: new Date(Date.now() - (60 * 5 * 10 * 1000)) } } });      // expires in 10 minutes
 
-        // await Tokens.Destroy({ where: { createdAt: { [Op.lt]: new Date(Date.now() - (60 * 60 * 24 * 1000)) } } });   //expire in 24 hrs
-        await Tokens.destroy({ where: { createdAt: { [Op.lt]: new Date(Date.now() - (60 * 5 * 1000)) } } });      // expires in 5 minutes
+    const user = await Users.findOne({ where: { userId: email } })
+    if (user) return res.status(400).json({ message: "Email is already taken" })
 
-        const user = await Users.findOne({ where: { userId: email } })
-        if (user) return res.status(400).json({ message: "Email is already taken" })
+    const verificationToken = jwt.sign({ name, email }, process.env.JWT_EMAIL_KEY, { expiresIn: "5m" });
+    const tempUser = { userId: email, name, password: hashedPassword, token: verificationToken, photo: file }
 
-        const verificationToken = jwt.sign({ name, email }, process.env.JWT_EMAIL_KEY, { expiresIn: "5m" });
-        const tempUser = { userId: email, name, password: hashedPassword, token: verificationToken, photo: file }
+    await sendMail(req.body.email, "Verify Email on localhost", verificationToken, "verification")
 
-        await sendMail(req.body.email, "Verify Email on localhost", verificationToken, "verification")
+    await Tokens.create(tempUser)
+    res.status(200).send(tempUser)
 
-        await Tokens.create(tempUser)
-        res.status(200).send(tempUser)
-
-    } catch (e) {
-        res.status(500).json({ message: (e.message || "Please try again later") })
-    }
 }
 
 module.exports = { tempUser };
