b5e2cc2127c953565a12b873deff5d1451d09804
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-02-19 01:20:44 +0500

    textfield animation added

--- client/src/misc/MUIComponents.js
+++ client/src/misc/MUIComponents.js
@@ -81,6 +81,24 @@ const StyledField = styled(TextField)(({ error }) => ({
     "& .MuiOutlinedInput-notchedOutline": {
         border: "0.5px solid black",
         background: "rgb(180, 180, 180, 0.2)",
+        animation: error && "shake 500ms ease-in-out",
+        "@keyframes shake": {
+            "0%": {
+                transform: "translateX(0)",
+            },
+            "25%": {
+                transform: "translateX(-5px)",
+            },
+            "50%": {
+                transform: "translateX(5px)",
+            },
+            "75%": {
+                transform: "translateX(-5px)",
+            },
+            "100%": {
+                transform: "translateX(0)",
+            },
+        },
     },
     "&.Mui-focused": {
         "& .MuiOutlinedInput-notchedOutline": {
