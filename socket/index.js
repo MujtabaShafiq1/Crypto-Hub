39d31d013a178bd4e2359f429bb37de27cfa27fe
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-03-04 10:04:21 +0500

    socket secure connection added

--- socket/index.js
+++ socket/index.js
@@ -1,37 +1,30 @@
-const io = require("socket.io")(8900, {
-    cors: { origin: "http://localhost:3000", credentials: true },
-});
-
+const io = require("socket.io")(8900, { cors: { origin: "http://localhost:3000", credentials: true } });
 const cookieSession = require("cookie-session");
-const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const users = new Map();
 
-// Set up cookie-parser middleware
 io.use((socket, next) => {
-    const sessionMiddleware = cookieSession({
-        name: "session",
-        keys: [process.env.COOKIE_KEY],
-        maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    });
+    const sessionMiddleware = cookieSession({ name: "session", keys: [process.env.COOKIE_KEY], maxAge: 24 * 60 * 60 * 1000 });
 
     sessionMiddleware(socket.request, {}, () => {
         const session = socket.request.session;
-        if (!session?.passport?.user?.id) return next(new Error("Invalid User"))
+        if (!session?.passport?.user?.id) return next(new Error("Invalid User"));
+        users.set(socket.id, session.passport.user.id)
         next();
     });
 });
 
 io.on("connection", (socket) => {
     // when user connects to server
-    const id = socket.request.session.passport.user.id;
-    console.log("New socket connection", id);
+    console.log("New socket connection");
+    console.log(users)
 
     // when user disconnects from server
     socket.on("disconnect", () => {
-        // users.delete(socket.id);
-        console.log("User disconnection");
+        users.delete(socket.id);
+        console.log("User disconnection", socket.id);
+        console.log(users)
         // socket.broadcast.emit("disconnectedUser", socket.id);
     });
 });
