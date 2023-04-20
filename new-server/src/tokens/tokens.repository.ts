1519b725b75db5da4575b83ee1044099073f1f44
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-04-20 23:22:52 +0500

    token verification added

--- new-server/src/tokens/tokens.repository.ts
+++ new-server/src/tokens/tokens.repository.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { NotFoundException } from '@nestjs/common/exceptions';
 import { Repository } from 'typeorm';
-
-import { JwtService } from '@nestjs/jwt';
-import { JwtPayload } from '../auth/jwt/jwt-payload.interface';
-import * as bcrypt from 'bcrypt';
 
 // Entity
 import { Token } from './token.entity';
@@ -19,18 +14,12 @@ export class TokensRepository extends Repository<Token> {
   constructor(
     @InjectRepository(Token)
     private tokensRepository: Repository<Token>,
-
-    private jwtService: JwtService,
   ) {
     super(
       tokensRepository.target,
       tokensRepository.manager,
       tokensRepository.queryRunner,
     );
-  }
-
-  generateJwt(payload: JwtPayload) {
-    return this.jwtService.sign(payload);
   }
 
   // find token
@@ -48,6 +37,16 @@ export class TokensRepository extends Repository<Token> {
     return foundToken;
   }
 
+  // create token
+  async createToken(
+    user: RegisterLocalUserDto,
+    token: UserTokenDto,
+  ): Promise<Token> {
+    const newToken = this.tokensRepository.create({ ...user, ...token });
+    const savedToken = await this.tokensRepository.save(newToken);
+    return savedToken;
+  }
+
   // delete token
   async deleteToken(userToken: UserTokenDto): Promise<void> {
     await this.tokensRepository
@@ -55,33 +54,5 @@ export class TokensRepository extends Repository<Token> {
       .delete()
       .where('token.token LIKE :token', { token: `${userToken.token}` })
       .execute();
-  }
-
-  // create token
-  async createToken(user: RegisterLocalUserDto): Promise<Token> {
-    const { username, password, ...otherDetails } = user;
-
-    // Hash the password and token
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const token = this.generateJwt({ username });
-
-    // Storing the record
-    const newToken = this.tokensRepository.create({
-      username,
-      password: hashedPassword,
-      token,
-      ...otherDetails,
-    });
-    const savedToken = await this.tokensRepository.save(newToken);
-    return savedToken;
-  }
-
-  // verify token
-  async verifyToken(userToken: UserTokenDto): Promise<Token> {
-    // verify token as well here
-    const foundUser = await this.findToken(userToken);
-    if (!foundUser) throw new NotFoundException('Invalid Token');
-    return foundUser;
   }
 }
--- new-server/src/tokens/tokens.service.ts
+++ new-server/src/tokens/tokens.service.ts
@@ -1,28 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { TokensRepository } from './tokens.repository';
 import { Token } from './token.entity';
+import { JwtService } from '@nestjs/jwt';
+import { plainToClass } from 'class-transformer';
+import * as bcrypt from 'bcrypt';
+
+// Exceptions
+import {
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common/exceptions';
 
 // DTO
 import { UserTokenDto } from 'src/users/dto/user-token.dto';
 import { RegisterLocalUserDto } from 'src/users/dto/register-local-user-dto';
+import { JwtPayload } from 'src/auth/jwt/jwt-payload.interface';
 
 @Injectable()
 export class TokensService {
-  constructor(private tokensRepository: TokensRepository) {}
+  constructor(
+    private jwtService: JwtService,
+    private tokensRepository: TokensRepository,
+  ) {}
 
-  async findToken(userToken: UserTokenDto): Promise<Token> {
-    return this.tokensRepository.findToken(userToken);
+  async verifyToken(userToken: UserTokenDto): Promise<Token> {
+    try {
+      this.jwtService.verify(userToken.token);
+      const foundUser = await this.tokensRepository.findToken(userToken);
+      if (!foundUser) throw new NotFoundException('Invalid Token');
+      return foundUser;
+    } catch (e) {
+      throw new NotAcceptableException('Token Expired , Sign-up again');
+    }
   }
 
   async createToken(user: RegisterLocalUserDto): Promise<Token> {
-    return this.tokensRepository.createToken(user);
+    // Hash the password and token
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    const payload: JwtPayload = { username: user.username };
+
+    const newToken: string = this.jwtService.sign({ payload });
+    const plainTokenObject = { token: newToken };
+    const savedToken = plainToClass(UserTokenDto, plainTokenObject);
+
+    user.password = hashedPassword;
+    return this.tokensRepository.createToken(user, savedToken);
   }
 
   async deleteToken(userToken: UserTokenDto): Promise<void> {
     return this.tokensRepository.deleteToken(userToken);
-  }
-
-  async verifyToken(user: UserTokenDto): Promise<Token> {
-    return this.tokensRepository.verifyToken(user);
   }
 }
